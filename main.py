80081421cf2392973628f84ac27c7328f3d0c554
Dev-98 <devparker48@gmail.com>
2024-01-26 22:15:37 +0530

    Update

--- main.py
+++ main.py
@@ -16,53 +16,67 @@ CORS(app)
 def main():
 	return ' HELLO WORLD '
 
+
 @app.route('/predict', methods=['GET','POST'])
 def predict():
 
 	if request.method == 'POST':
 		image = request.files['image']
-	
-		path = Image.open(image.stream)
-		reader = easyocr.Reader(['en'])
 
-		results = reader.readtext(path)
-		
-		text = ' ' 
-		for result in results:
-			text += result[1] + ' '
+		try:
+			path = Image.open(image.stream)
+			reader = easyocr.Reader(['en'])
 
-		# print(p,'\n',text,'\n',p)
-
-		remove_digits = str.maketrans('', '', punctuation)
-		remove_digits2 = str.maketrans('', '', digits)
-
-		res = text.translate(remove_digits)
-		res = res.translate(remove_digits2)
-
-		new_text = ""
-		for word in str(res).split():
-			if (len(word) > 4):
-				new_text += word + " "
-
-		# print(p,'\n',new_text,'\n',p)
-		data = pd.read_csv("medicine_data.csv")
-		
-		l=[]
-
-		for i in data["Medicine"]:
-			score = fuzz.partial_token_sort_ratio(i, new_text)
-			l.append(score)
-			index = sorted(list(enumerate(l)), reverse=True, key=lambda x: x[1])[0][0]
-			if max(l) >= 80:
-				bimari = data["Disease"][index]
-				dawai = data["Medicine"][index]
-				out = {'Ailments': bimari, 'Medicine': dawai}
-			else :
-				bimari = 'Not available in database'
-				dawai = 'Unreadable image'
-				out = {'Ailments': bimari, 'Medicine': dawai}
+			results = reader.readtext(path)
 			
-		return jsonify(out), 200
+			text = ' ' 
+			for result in results:
+				text += result[1] + ' '
+
+			# print(p,'\n',text,'\n',p)
+
+			remove_digits = str.maketrans('', '', punctuation)
+			remove_digits2 = str.maketrans('', '', digits)
+
+			res = text.translate(remove_digits)
+			res = res.translate(remove_digits2)
+
+			new_text = ""
+			for word in str(res).split():
+				if (len(word) > 4):
+					new_text += word + " "
+
+			# print(p,'\n',new_text,'\n',p)
+			data = pd.read_csv("medicine_data.csv")
+			
+			l=[]
+
+			print("Name = ",new_text.lower())
+
+			for i in data["Medicine_Name"]:
+				score = fuzz.partial_token_sort_ratio(i.lower(), new_text.lower())
+				l.append(score)
+				index = sorted(list(enumerate(l)), reverse=True, key=lambda x: x[1])[0][0]
+				# print("\n Score = ",max(l))
+				if max(l) >= 80:
+					uses = data["Uses"][index]
+					dawai = data["Medicine_Name"][index]
+					effects = data["Side_effects"][index]
+					url = data["Image URL"][index]
+
+					out = {'Use cases': uses, 'Medicine': dawai, 'effects': effects, 'image_url': url}
+
+				else :
+					
+					not_found = 'Not available in database, Unreadable image'
+
+					out = {'Not Found': not_found}
+
+			return jsonify(out), 200
+
+		except Exception as e :
+
+			return jsonify({'error': e}), 500
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
